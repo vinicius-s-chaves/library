b1959bac2c9d56b075db2621da39001f3c5f0c29
Vinicius Chaves <228245525+vinicius-s-chaves@users.noreply.github.com>
2026-02-11 19:16:06 -0300

    fix: handle informations of the book form to add it to library

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const body = document.querySelector('body')
+const bookForm = document.querySelector('#bookForm')
 const myLibrary = []
 
 function Book(title, author, pages, read) {
@@ -15,10 +17,24 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBooks() {
-    let body = document.querySelector('body')
-    for (book of myLibrary) {
+    for (let book of myLibrary) {
         let displayedBook = document.createElement('div')
         displayedBook.textContent = book.info
         body.appendChild(displayedBook)
     }
 }
+
+bookForm.addEventListener('submit', submitBookInfo)
+
+function submitBookInfo(e) {
+    let title = document.querySelector('#inTitle').value
+    let author = document.querySelector('#inAuthor').value
+    let pages = document.querySelector('#inPages').value
+    // let read = document.querySelector('#readed').value
+
+    addBookToLibrary(title, author, pages, 'yes')
+    displayBooks()
+    e.preventDefault()
+}
+
+displayBooks()
