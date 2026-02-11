3e9c376dd29dc0c8583813d0a39f2e1c8dc8b02b
Vinicius Chaves <228245525+vinicius-s-chaves@users.noreply.github.com>
2026-02-11 10:43:38 -0300

    feat: display books on the page

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
     this.id = crypto.randomUUID()
+    this.info = `${title}, by ${author}, ${pages} pages. Read: ${read}`
 }
 
 function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read)
     myLibrary.push(book)
+}
+
+function displayBooks() {
+    let body = document.querySelector('body')
+    for (book of myLibrary) {
+        let displayedBook = document.createElement('div')
+        displayedBook.textContent = book.info
+        body.appendChild(displayedBook)
+    }
 }
